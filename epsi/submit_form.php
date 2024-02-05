58b7b9a94b9a3f64fee8e387b93b7f82c383abb5
okiod <nathan.leituga@gmail.com>
2024-02-05 15:18:43 +0100

    plus le mode debug

--- epsi/submit_form.php
+++ epsi/submit_form.php
@@ -1,8 +1,6 @@
 <?php
 // submit_form.php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 // Vérifie si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
