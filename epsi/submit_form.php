f82963381595e91ed44254ef97790394e1f6822e
okiod <nathan.leituga@gmail.com>
2024-02-05 11:34:48 +0100

    Rajout de la redirection

--- epsi/submit_form.php
+++ epsi/submit_form.php
@@ -6,24 +6,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Chemin du fichier CSV où les données seront sauvegardées
     $csvFilePath = "data.csv";
 
-
     // Ouvre le fichier CSV en mode append
     $csvFile = fopen($csvFilePath, 'a');
 
     // Si le fichier n'existe pas, crée l'entête du CSV
     if (filesize($csvFilePath) == 0) {
         fputcsv($csvFile, [
-            'Nom', 'Prénom', 'Adresse', 'Ville', 'Code Postal', 'Email', 
+            'Nom', 'Prénom', 'Ville', 'Code Postal', 'Email', 
             'Téléphone', 'Niveau BAC', 'Section/Spécialité', 'Etablissement', 
             'Contact par Email', 'Contact par SMS', 'Date', 'Formations Intéressées', 
-            'Consentement', 'Signature'
+            'Consentement'
         ]);
     }
 
     // Récupère les données du formulaire
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
-    $adresse = $_POST['adresse'];
     $ville = $_POST['ville'];
     $codePostal = $_POST['code_postal'];
     $email = $_POST['email'];
@@ -37,11 +35,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Concaténez toutes les formations choisies en une chaîne
     $formations = implode(', ', array_filter($_POST['formation'] ?? [], fn($value) => !empty($value)));
     $consentement = isset($_POST['consentement']) ? 'Oui' : 'Non';
-    // Vous devrez implémenter la capture et le traitement de la signature ici
 
     // Créer une ligne de données pour le CSV
     $formData = [
-        $nom, $prenom, $adresse, $ville, $codePostal, $email,
+        $nom, $prenom, $ville, $codePostal, $email,
         $telephone, $niveauBac, $section, $etablissement,
         $contactEmail, $contactSms, $date, $formations,
         $consentement
@@ -49,13 +46,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Écriture des données dans le fichier CSV
     fputcsv($csvFile,$formData);
-// Fermeture du fichier
-fclose($csvFile);
+    // Fermeture du fichier
+    fclose($csvFile);
 
-// Message de confirmation ou redirection
-echo "Les informations ont été enregistrées avec succès.";
-// Ou vous pouvez rediriger vers une autre page avec:
-// header('Location: merci.html');
-// exit();
+    // Redirection après 5 secondes
+    header("Refresh:5; url=index.html");
+
+    // Message de confirmation
+    echo "Les informations ont été enregistrées avec succès. Vous allez être redirigé vers la page d'accueil dans 5 secondes.";
 }
 ?>
